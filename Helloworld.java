66faeb49c769884d657285e7215b9babdaaf913b
mr-sarathi <sarathijothi697@gmail.com>
2026-01-29 11:55:59 +0530

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,7 @@
+class Helloworld
+{
+ public static void main(String a[])
+ {
+System.out.println ("Hello world");
+ }
+}
---
+++ addition.java
@@ -0,0 +1,7 @@
+class Addition 
+{
+ public static void main(String a[])
+ {
+System.out.println("25+25="+(25+25));
+ }
+}
